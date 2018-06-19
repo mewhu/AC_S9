03096b612dcb6fe24726f6b0422943574beb832b
mewhu <mew31716@gmail.com>
2018-06-20 02:01:47 +0800

    creating migration for add new column "photographer"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180619160023) do
+ActiveRecord::Schema.define(version: 20180619175122) do
 
   create_table "photos", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180619160023) do
     t.string "file_location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photographer"
   end
 
 end
