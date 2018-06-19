0fa69117aa11edbbe2b604ea3ab6c5ef6e3382b4
mewhu <mew31716@gmail.com>
2018-06-20 04:39:09 +0800

    add "index" action in controller : passing all photo data stored in DB via instance variable @photos manipulated by model "Photo"

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,7 @@
 class PhotosController < ApplicationController
+  #定義首頁檔(for the controller-action "index")
+  #在這裡 Photo.all 這個指令會回傳所有在我們的表格資料庫中的相片，以及被指派到 @photos 這個實例變數 (也就是有 @ 開頭的變數) 當中。
+  def index
+    @photos = Photo.all
+  end
 end
