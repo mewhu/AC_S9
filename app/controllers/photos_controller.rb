d47f689181c8ce907053c853e3877c1bbdf45408
mewhu <mew31716@gmail.com>
2018-06-26 03:18:20 +0800

    refactor to a more cleaner controller code

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,32 +1,17 @@
 class PhotosController < ApplicationController
-  # 用before_action這個功能，在:show, :edit, :update及:destroy等處設定 private function "target_photo" 的callback
-  # PS: 舊版rails是用before_filter
   before_action :target_photo, :only => [:show, :edit, :update, :destroy]
 
   
-  ####################################################
-  ########## 以下為controller action "index" ##########
-  ####################################################
-  #定義首頁檔(for the controller-action "index")
-  #在這裡 Photo.all 這個指令會回傳所有在我們的表格資料庫中的相片，以及被指派到 @photos 這個實例變數 (也就是有 @ 開頭的變數) 當中。
   def index
     @photos = Photo.all
   end
 
 
-  ##############################################################
-  ########## 以下為controller action "new" 及 "create" ##########
-  ##############################################################
-  #定義new頁面
   def new
-    #叫controller用model "Photo"中的new方法去取得資料，並且用@photo實例變數接住，送去給new頁面
     @photo = Photo.new
   end
 
-  #def create及def photo_parms : 使"new"頁面最後的submit-button可以真正送data到rails-web裡
-  #def photo_params使用private method式的定義
-  #把讀取 params 的過程包在 photo_params 裡，再從 create action 裡呼叫，送給Photo.new 做為參數。
-  #photos_url : index-action的RESTful prefix "photos" 配上 "_url", 可以産生index首頁的URL位置
+
   def create
     @photo = Photo.new(photo_params)
 
@@ -38,30 +23,9 @@ class PhotosController < ApplicationController
   end
 
 
-  ###################################################
-  ########## 以下為controller action "show" ##########
-  ###################################################
-  def show
-    #叫controller用model "Photo"中的find(params[:id])方法去取得資料，並且用@photo實例變數接住，送去給show頁面
-    # 以before_action設定callback後，即可把原先的程式碼mark down
-    # @photo = Photo.find(params[:id])
-  end
-
-
-  ###############################################################
-  ########## 以下為controller action "edit" 及 "update" ##########
-  ###############################################################
-  def edit
-    # 以before_action設定callback後，即可把原先的程式碼mark down
-    # @photo = Photo.find(params[:id])
-  end
-
   def update
-    # 以before_action設定callback後，即可把原先的程式碼mark down
-    # @photo = Photo.find(params[:id])
 
     if @photo.update_attributes(photo_params)
-      #設定update結束後將頁面導到該@photo的"show"頁面
       redirect_to photo_path(@photo)
     else
       render :action => :edit
@@ -69,12 +33,7 @@ class PhotosController < ApplicationController
   end
 
 
-  ######################################################
-  ########## 以下為controller action "destroy" ##########
-  ######################################################
   def destroy
-    # 以before_action設定callback後，即可把原先的程式碼mark down
-    # @photo = Photo.find(params[:id])
     @photo.destroy
     redirect_to photos_url
   end
@@ -85,8 +44,7 @@ class PhotosController < ApplicationController
     params.require(:photo).permit(:title, :date, :description, :file_location)
   end
 
-  # 建立private方法"target_photo"，配合before_action做callback，
-  # 以置換掉:show, :edit, :update, :destroy中的重置字眼，符合DRY原則
+  
   def target_photo
     @photo = Photo.find(params[:id])
   end
